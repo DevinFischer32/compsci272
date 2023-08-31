b2905a8492e7eef9b5577007713db5639b26d7da
Devin Fischer <devinfischer32@gmail.com>
2023-08-31 10:15:25 -0700

    added addNum Function

--- firstDay.cpp
+++ firstDay.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
-int main(int argc, char args[])
+int addNum(int num1, int num2)
 {
+    return num1 + num2;
+}
 
+int main(int argc, char *args[])
+{
+    int val1 = 23;
+    int val2 = 45;
+    int sum = addNum(val1, val2);
     return 0;
 }
---
+++ note.md
@@ -0,0 +1,3 @@
+passing a parameter passes a low level version of the varible not memory space.
+
+& sends memory location of varible
