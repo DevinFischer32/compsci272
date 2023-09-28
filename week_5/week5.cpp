66fc2c5572d2dfa1ede4d0794d515c15846018d5
Devin Fischer <devinfischer32@gmail.com>
2023-09-28 10:47:32 -0700

    Calc

--- week_5/week5.cpp
+++ week_5/week5.cpp
@@ -6,5 +6,34 @@ using namespace std;
 
 int main()
 {
+    int num1, num2;
+    char symbol;
+
+    cout << "Enter Number 1: " << endl;
+    cin >> num1;
+    cout << "Enter Number 2: " << endl;
+    cin >> num2;
+    cout << "Enter (+ - / *): " << endl;
+    cin >> symbol;
+
+    if (symbol == '+')
+    {
+        return num1 + num2;
+    }
+    else if (symbol == '-')
+    {
+        return num1 - num2;
+    }
+    else if (symbol == '/')
+    {
+        return num1 / num2;
+    }
+    else if (symbol == '*')
+    {
+        return num1 * num2;
+    }
+    else
+        cout << "Please Enter Valid Symbol" << endl;
+
     return 0;
 }
