397c3d81a50b489038e5f971b0d1b228aa49c306
Devin Fischer <devinfischer32@gmail.com>
2023-09-28 09:50:05 -0700

    Setting up for week 5 class of CS272

---
+++ week_5/Classes.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Classes
+{
+private:
+public:
+    Classes();
+    ~Classes();
+};
+
+Classes::Classes()
+{
+}
+
+Classes::~Classes()
+{
+}
